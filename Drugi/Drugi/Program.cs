30de53083b8aa4d40dd2dc43171c79a280d86568
Zan Puc <zan.puc@gmail.com>
2023-01-20 10:25:00 +0100

    Vaja za git

--- Drugi/Drugi/Program.cs
+++ Drugi/Drugi/Program.cs
@@ -10,10 +10,28 @@ namespace Drugi
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(" Stari Donald farmo ima");
+
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("IAIAO");
             Console.WriteLine("na tej farmi osla dva");
             Console.WriteLine("IAIAO");
+            Console.WriteLine("Mism zelo lepo da ima Donald kmetijo");
+            Console.WriteLine("Ma ima tudi Donald traktor");
+            Console.WriteLine("Enej je pevec");
+            Console.WriteLine("Nace basist");
+            Console.WriteLine("Kekci");
+            
+
+            Console.WriteLine("IAIAO");
+            
+
+
+
+
+
+
 
 
 
