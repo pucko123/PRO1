e79f7b637c423a9e39c25b1e273125f47909d130
Zan Puc <zan.puc@gmail.com>
2023-01-20 11:01:35 +0100

    Napad na Trst

--- Drugi/Drugi/Program.cs
+++ Drugi/Drugi/Program.cs
@@ -10,21 +10,28 @@ namespace Drugi
     {
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Magenta;
             Console.WriteLine(" Stari Donald farmo ima");
 
-            Console.ForegroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("IAIAO");
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("na tej farmi osla dva");
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("IAIAO");
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Mism zelo lepo da ima Donald kmetijo");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
             Console.WriteLine("Ma ima tudi Donald traktor");
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("Enej je pevec");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
             Console.WriteLine("Nace basist");
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Kekci");
-            
-
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("IAIAO");
+
             
 
 
